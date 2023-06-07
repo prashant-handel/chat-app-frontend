3fdfee0375e38e6d5a10a5b90b669dbd95023763
prashant_handel <prashanthandel2501@gmail.com>
2023-06-07 23:18:57 +0530

    Signed-off-by: prashant_handel <prashanthandel2501@gmail.com>

---
+++ src/pages/Login.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import './Login.css'
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return (
+        <Container>
+            <Row>
+                <Col md={5} className="login__bg">
+                </Col>
+                <Col md={5} className="d-flex align-items-center justify-content-center flex-direction-column">
+                <Form style={{width: '80%', maxWidth: '500'}}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+                    <div className="py-4">
+                        <p className="text-center">
+                            Don't have an account <Link to='./signup'>Sign Up</Link>
+                        </p>
+                    </div>
+                </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Login;
